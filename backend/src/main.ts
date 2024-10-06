64b72a108575e906c98c94f8dc1f6bec14b677b3
Oguzhan <oguzhanbaysal@outlook.com>
2024-10-06 17:38:08 +0300

    Fix startup configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-   import { AppModule } from './app.module';
+import { AppModule } from './app.module';
 
-   async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: ['http://localhost:3000', 'https://your-frontend-url.com'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
-    await app.listen(process.env.PORT || 3000);
-  }
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
