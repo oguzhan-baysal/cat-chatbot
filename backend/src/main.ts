97b0ebf91df2500516de20676e3dfe735525e816
Oguzhan <oguzhanbaysal@outlook.com>
2024-10-06 16:38:12 +0300

    Configure for Heroku deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
    async function bootstrap() {
      const app = await NestFactory.create(AppModule);
-     app.enableCors();
-     await app.listen(process.env.PORT || 3000);
+     app.enableCors(); // Bu satırın eklendiğinden emin olun
+     await app.listen(3000);
    }
    bootstrap();
